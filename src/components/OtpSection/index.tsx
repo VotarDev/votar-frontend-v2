73925175146c41ed7dc5c07d0e98f2701e4722e7
Tobi Faniran <tobifaniran@gmail.com>
2024-04-13 12:50:41 +0100

    fix: bug fixes

--- src/components/OtpSection/index.tsx
+++ src/components/OtpSection/index.tsx
@@ -47,7 +47,7 @@ const OtpSection = () => {
 
   const handleVerifyOtp = async (e: React.FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    if (user) setAuthToken(user.data ? user.data.data.cookie : null);
+    // if (user) setAuthToken(user.data ? user.data.data.cookie : null);
     setLoading(true);
     setSuccess(false);
     const otpString = otp.join("");
