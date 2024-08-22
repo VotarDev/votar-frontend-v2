3f6b741d17da5faf04f8dda1110af06d6ec66aa3
Tobi Faniran <tobifaniran@gmail.com>
2024-08-22 19:58:34 +0100

    feat: new update

--- src/components/Admin/AdminProfile/SwitchButton/index.tsx
+++ src/components/Admin/AdminProfile/SwitchButton/index.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { adminPublishElection } from "@/utils/api";
+import Cookies from "universal-cookie";
+import toast from "react-hot-toast";
 
-const SwitchButton = () => {
+const SwitchButton = ({ id, row }: any) => {
+  const handleChange = () => {};
   return (
     <div>
       <label className="toggle">
-        <input className="toggle-checkbox" type="checkbox" />
+        <input
+          className="toggle-checkbox"
+          type="checkbox"
+          onChange={handleChange}
+        />
         <div className="toggle-switch"></div>
       </label>
     </div>
